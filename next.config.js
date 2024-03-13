642a896fe8708dba170e3313d909173d9747467e
Mypmansk <mypmansk@mail.ru>
2024-03-13 15:10:38 +0300

    Added project on Github

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    domains: ["old-images.hb.ru-msk.vkcs.cloud"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
